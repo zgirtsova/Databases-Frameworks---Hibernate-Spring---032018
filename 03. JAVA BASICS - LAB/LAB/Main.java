3f3d5de3b81f3368870e8c59afb171bf99c777f7
Midori_san <zgirtsova@gmail.com>
2018-03-07 13:35:46 +0200

    New files

---
+++ 03. JAVA BASICS - LAB/LAB/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+	public static void main(String[] args){
+	
+		System.out.print("Hello, ");
+		System.out.print(args[0]);
+		System.out.println("!");
+	}
+
+}
---
+++ 03. JAVA BASICS - LAB/LAB/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.math.BigDecimal;
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        
+    }
+}
